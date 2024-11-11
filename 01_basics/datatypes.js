8a317328279ff36d248e807245b924a7b0abd526
akshitasingh23 <161047073+akshitasingh23@users.noreply.github.com>
2024-11-11 07:02:12 +0000

    datatyes and vafriables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,11 @@
+//alert( 3 + 3) //we are using nodejs,not browser 
+let name ="akshita"
+let age = 21
+let isLoggedIn=false
+// Number
+// BigInt
+// string
+// Boolean
+// null
+// undefined
+// Symbol
---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+const account_id = 2003
+let account_email ="forever98@gmail.com"
+var password="apple21"
+accountcity="ghazipur"
+
+// account_id = 2 // not allowed
+account_email="aksh98@gmail.com"
+password ="123@ak"
+accountcity="modiour"
+console.table([account_email,password,accountcity]);

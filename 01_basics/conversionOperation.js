d0f229601c4b4645747d37491860665ebbcc96a9
akshitasingh23 <161047073+akshitasingh23@users.noreply.github.com>
2024-11-11 09:16:29 +0000

    conversiom

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,10 +1,10 @@
-let score = "33mnc"
-console.log(typeof score);
-console.log(typeof(score));
+// let score = "33mnc"
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber= Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 // "33"=>33
 // "33abc"=>Nan
@@ -12,7 +12,9 @@ console.log(valueInNumber)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 // 1=>true; 0=>false
 // '' ""=>false
 // "hitesh"=true
+
+// console.log((3+4) * (5 % 3) )
